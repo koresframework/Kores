01b12ebc7110f7ad0ef9a209556649f1e6a3a30e
Jonathan <joniweb01@gmail.com>
2017-02-21 15:29:49 -0300

    Fixed LocalClassTest

--- src/test/java/com/github/jonathanxd/codeapi/test/LocalClassTest_.java
+++ src/test/java/com/github/jonathanxd/codeapi/test/LocalClassTest_.java
@@ -29,14 +29,18 @@ package com.github.jonathanxd.codeapi.test;
 
 import com.github.jonathanxd.codeapi.CodeAPI;
 import com.github.jonathanxd.codeapi.CodeSource;
+import com.github.jonathanxd.codeapi.MutableCodeSource;
 import com.github.jonathanxd.codeapi.Types;
+import com.github.jonathanxd.codeapi.base.Annotation;
 import com.github.jonathanxd.codeapi.base.TypeDeclaration;
 import com.github.jonathanxd.codeapi.common.CodeModifier;
 import com.github.jonathanxd.codeapi.factory.ClassFactory;
 import com.github.jonathanxd.codeapi.factory.ConstructorFactory;
 import com.github.jonathanxd.codeapi.factory.MethodFactory;
+import com.github.jonathanxd.codeapi.generic.GenericSignature;
 import com.github.jonathanxd.codeapi.helper.Predefined;
 import com.github.jonathanxd.codeapi.literal.Literals;
+import com.github.jonathanxd.codeapi.type.CodeType;
 import com.github.jonathanxd.iutils.annotation.Named;
 import com.github.jonathanxd.iutils.object.Pair;
 
@@ -47,13 +51,25 @@ public class LocalClassTest_ {
     //
     public static Pair<@Named("Main class") TypeDeclaration, @Named("Source") CodeSource> $() {
 
-        TypeDeclaration local = ClassFactory.aClass(EnumSet.noneOf(CodeModifier.class), "test.Greeter", CodeAPI.sourceOfParts(
+        MutableCodeSource source = new MutableCodeSource();
+
+        TypeDeclaration codeClass = ClassFactory.aClass(EnumSet.of(CodeModifier.PUBLIC), "test.LocalClass", source);
+
+        TypeDeclaration local = ClassFactory.aClass(
+                codeClass,
+                new Annotation[0],
+                EnumSet.noneOf(CodeModifier.class),
+                "test.Greeter",
+                GenericSignature.empty(),
+                Types.OBJECT,
+                new CodeType[0],
+                CodeAPI.sourceOfParts(
                 MethodFactory.method(EnumSet.of(CodeModifier.PUBLIC), "greet", Types.VOID, CodeAPI.sourceOfParts(
                         Predefined.invokePrintlnStr(Literals.STRING("Hello"))
                 ))
         ));
 
-        TypeDeclaration codeClass = ClassFactory.aClass(EnumSet.of(CodeModifier.PUBLIC), "test.LocalClass", CodeAPI.sourceOfParts(
+        source.addAll(CodeAPI.sourceOfParts(
 
                 ConstructorFactory.constructor(EnumSet.of(CodeModifier.PUBLIC), CodeAPI.sourceOfParts(
                         local,
