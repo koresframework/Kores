b94c950f8f5b28d0de8786e9beaefa4b44001119
Jonathan <joniweb01@gmail.com>
2017-05-15 21:26:23 -0300

    TypeKey util

--- src/main/kotlin/com/github/jonathanxd/codeapi/util/TypedDataUtil.kt
+++ src/main/kotlin/com/github/jonathanxd/codeapi/util/TypedDataUtil.kt
@@ -30,6 +30,7 @@ package com.github.jonathanxd.codeapi.util
 import com.github.jonathanxd.iutils.`object`.Pair
 import com.github.jonathanxd.iutils.`object`.TypedKey
 import com.github.jonathanxd.iutils.data.TypedData
+import com.github.jonathanxd.iutils.type.ConcreteTypeInfo
 import com.github.jonathanxd.iutils.type.TypeInfo
 import java.util.Optional
 
@@ -68,3 +69,10 @@ fun <T> TypedData.containsAnyKey(key: TypedKey<T>): Boolean {
     return this.contains(key.key)
 }
 
+// TypedKeyUtils
+
+/**
+ * Creates a [TypeInfo] of type [T].
+ */
+inline fun <reified T> typedKeyOf(key: Any): TypedKey<T> =
+        TypedKey<T>(key, object : ConcreteTypeInfo<T>(){})
